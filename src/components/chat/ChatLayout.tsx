543c2543a918deddf5a6d7b39fba07fce961c5e8
Jayce Nga <jayceenga@gmail.com>
2025-02-28 18:00:40 +0800

    .

--- src/components/chat/ChatLayout.tsx
+++ src/components/chat/ChatLayout.tsx
@@ -1,4 +1,5 @@
 "use client"
+import { useState } from "react";
 import { ResizableHandle, ResizablePanel, ResizablePanelGroup } from "../ui/resizable";
 
 interface ChatLayoutProps {
@@ -6,17 +7,22 @@ interface ChatLayoutProps {
 }
 
 const ChatLayout = ({ defaultLayout = [320,480] }: ChatLayoutProps) => {
+    const [isMobile, setIsMobile] = useState(false);
   return (
-    <ResizablePanelGroup 
-    direction={"horizontal"}
-    className="h-full items-stretch bg-background rounded-lg"
-    onLayout = {(sizes:number[]) => {
-        document.cookie = `react-resizable-panels:layout=${JSON.stringify(sizes)}`
-    }}
-    > 
-    <ResizablePanel>Sidebar</ResizablePanel>
-    <ResizableHandle withHandle/>
-    <ResizablePanel>right</ResizablePanel>
+    <><ResizablePanelGroup
+          direction={"horizontal"}
+          className="h-full items-stretch bg-background rounded-lg"
+          onLayout={(sizes: number[]) => {
+              document.cookie = `react-resizable-panels:layout=${JSON.stringify(sizes)}`;
+          } }
+      >
+          <ResizablePanel
+              defaultSize={defaultLayout[0]}
+              collapsedSize={8}
+              collapsible={true}
+              minSize={isMobile ? 0 : 24} />
+          
+          Sidebar</ResizablePanel><ResizableHandle withHandle /><ResizablePanel>right</ResizablePanel></>
 
     </ResizablePanelGroup>
   )
