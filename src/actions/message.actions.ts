46946a50bfda16320a1358e11080d4e847d35e02
JC Fella <jayceenga@gmail.com>
2025-03-04 23:01:20 +0800

    Update message.actions.ts

--- src/actions/message.actions.ts
+++ src/actions/message.actions.ts
@@ -1,4 +1,5 @@
 "use server";
+import { redis } from "@/lib/db";
 import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server";
 
 type SendMessageActionArgs = {
@@ -16,7 +17,27 @@ export async function sendMessageAction({content, messageType, receiverId}: Send
     }
 
     const senderId = user.id; 
-    const conversationId = `conversation:${senderId}:${receiverId}`;
+    const conversationId = `conversation:${[senderId, receiverId].sort().join(":")}`;
+    const conversationExists = await redis.exists(conversationId); 
+    
+    if(!conversationExists){
+        await redis.hset(conversationId, {
+            participant1: senderId,
+            participant2: receiverId
+        })
+        await redis.sadd(`user:${senderId}:conversations`, conversationId)
+        await redis.sadd(`user:${receiverId}:conversations`, conversationId)
+    }
+    // Generate unique message id
+    const messageId = `message:${Date.now()}:${Math.random().toString(36).substring(2,9)}`
+    const timestamp = Date.now()
 
+    await redis.hset(messageId, {
+        senderId,
+        content,
+        timestamp,
+        messageType
+    })
+    await redis.zadd(`${conversationId}:messages`,{score:timestamp,member:JSON.stringify(messageId)})
     return { success: true, conversationId, message: "Message sent successfully" }
 }
